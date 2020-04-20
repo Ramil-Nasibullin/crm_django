702ca829e39346565ff9bb23a0757c3520229777
Ramil-Nasibullin <ramil-hamza@yandex.ru>
2020-04-20 18:08:49 +0300

    Исправил некоторые дефекты

--- crm/settings.py
+++ crm/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'you email'
-EMAIL_HOST_PASSWORD = 'you pass'
+EMAIL_HOST_USER = '**************'
+EMAIL_HOST_PASSWORD = '**************'
 
